e98f7dad8632e36c71581c04bb4b47dd886ea3c1
adrian salajan <adrian salajan>
2017-01-04 21:33:27 +0200

    Add state exercises

--- FPinScala/src/main/scala/main/chapter6/State.scala
+++ FPinScala/src/main/scala/main/chapter6/State.scala
@@ -2,22 +2,35 @@ package main.chapter6
 
 case class State[S,+A](run: S => (A,S))
 {
-//   def map[B](f: A => B): State[S, B] = {
-//      State((s) => {
-//         val (a, s2) = run(s)
-//         (f(a), s2)
-//      })
-//   }
+//   def map[B](f: A => B):State[S, B] = State(s => {
+//      val (a, s2) = run(s)
+//      (f(a), s2)
+//   })
 
-   def flatMap[B](f: A => State[S, B]): State[S, B] = State(
-      state => {
-         val (a, state2) = run(state)
-         f(a).run(state2)
-      }
-   )
+   def map[B](f: A => B):State[S, B] = flatMap(x => State.unit(f(x)))
+
+//   def map2[B, C](s2: State[S, B])(f: (A, B) => C): State[S, C] = State(s => {
+//      val (a, state1) = run(s)
+//      val (b, state2) = s2.run(state1)
+//      (f(a, b), state2)
+//   })
+
+   def map2[B, C](s2: State[S, B])(f: (A, B) => C): State[S, C] =
+      flatMap(a => s2.map(b => f(a,b)))
+
+   def flatMap[B](f: A => State[S, B]): State[S, B] = State(s => {
+      val (a, state1) = run(s)
+      f(a).run(state1)
+   })
+
 }
 
 //unit, map, map2, flatMap, and sequence.
 object State {
    def unit[S, A](a: A):State[S, A] = State((s) => (a, s))
+
+   def sequence[S, A](states: List[State[S, A]]): State[S, List[A]] = {
+      val z = State.unit[S, List[A]](List[A]())
+      states.reverse.foldLeft(z)((acc, nextState) => nextState.map2(acc)((i, ac) => i :: ac))
+   }
 }
--- FPinScala/src/test/scala/main/chapter6/TestState.scala
+++ FPinScala/src/test/scala/main/chapter6/TestState.scala
@@ -4,12 +4,43 @@ import main.Test
 
 class TestState extends Test {
 
+   val rng: RNG = SimpleRNG(2)
+   val random1: Int = rng.nextInt._1
+   val random2: Int = rng.nextInt._2.nextInt._1
 
    "unit" should "work" in {
       State.unit(3).run(SimpleRNG(2)) shouldBe (3, SimpleRNG(2))
+
+
    }
 
-//   "map" should "work" in {
-//      State.unit(3).map(_ * 2).run(SimpleRNG(2)) shouldBe (6, SimpleRNG(2))
-//   }
+   "map" should "work" in {
+      State.unit(3).map(_ * 2).run(SimpleRNG(2)) shouldBe (6, SimpleRNG(2))
+
+      State[RNG, Int](s => s.nextInt).map(_ * 2).run(rng)._1 shouldBe random1 * 2
+   }
+
+   "map2" should "work" in {
+      State.unit[String, Int](3).map2(State.unit[String, Int](4))(_ + _).run("x") shouldBe State.unit(7).run("x")
+
+      val map2 = State[RNG, Int](s => s.nextInt).map2(State[RNG, Int](s => s.nextInt))((a, b) => a + b + 1)
+      map2.run(rng)._1 shouldBe random1 + random2 + 1
+   }
+
+   "flatMap" should "work" in {
+      State.unit[String, Int](3).flatMap(x => State.unit[String, Int](x * 2)).run("x") shouldBe State.unit(6).run("x")
+
+      val fp = State[RNG, Int](s => s.nextInt)
+                  .flatMap(x => State[RNG, String](rng => (x.toString, SimpleRNG(x))))
+
+      fp.run(rng)._1 shouldBe random1.toString()
+   }
+
+   "sequence" should "work" in {
+      val rngState = State[RNG, Int](s => s.nextInt)
+
+      val actual = State.sequence(List(rngState, rngState)).run(rng)._1
+
+      actual shouldBe List(random1, random2)
+   }
 }
