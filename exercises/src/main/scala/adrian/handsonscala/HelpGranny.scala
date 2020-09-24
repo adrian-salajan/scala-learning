b81e553ccbd3bb9fc4f81d8d7d0aa8fa08dc4feb
AdrianS <adrian.salajan@ing.com>
2020-09-24 17:04:16 +0300

    Codewars: help granny

---
+++ exercises/src/main/scala/adrian/handsonscala/HelpGranny.scala
@@ -0,0 +1,39 @@
+package adrian.handsonscala
+
+//https://www.codewars.com/kata/5536a85b6ed4ee5a78000035/train/scala
+object HelpGranny extends App {
+//  val friends1 = List("A1", "A2", "A3", "A4", "A5")
+//  val fTowns1 = List(("A1", "X1"), ("A2", "X2"), ("A3", "X3"), ("A4", "X4"))
+//  val distTable1 = Map("X1" -> 100.0, "X2" -> 200.0, "X3" -> 250.0, "X4" -> 300.0)
+
+  def tour(arrFriends: List[String], ftoT: List[(String, String)], h: Map[String, Double]): Int = {
+    val friendsToTown = ftoT.toMap
+
+    val (lastCityVisited, dTraveled) = arrFriends.foldLeft(("homeBase", 0d)) { case ((lastLocation, dTraveled), nextFriend) =>
+        friendsToTown.get(nextFriend)
+          .fold(
+              (lastLocation, dTraveled)
+            ){nextLocation =>
+              if (lastLocation == "homeBase") {
+                (nextLocation, dTraveled + h(nextLocation))
+              } else {
+                (nextLocation, dTraveled + length(lastLocation, nextLocation, h))
+              }
+            }
+    }
+
+    Math.floor(dTraveled + h(lastCityVisited)).toInt
+
+  }
+
+  def length(loc1: String, loc2: String, h:Map[String, Double]): Double = {
+    val d1 = h(loc1)
+    val d2 = h(loc2)
+    Math.sqrt {
+      if (d2 > d1)
+        (d2 * d2) - (d1 * d1)
+      else
+        (d1 * d1) - (d2 * d2)
+    }
+  }
+}
